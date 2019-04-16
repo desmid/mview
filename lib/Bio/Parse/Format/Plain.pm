c00fd936b5002bf8558d3660cb7595cddc18d62e
nige <nigebrown@gmail.com>
2019-04-16 17:33:16 +0200

    Plain: reorganize regexp constants; add many test warnings (disabled)

--- lib/Bio/Parse/Format/Plain.pm
+++ lib/Bio/Parse/Format/Plain.pm
@@ -16,18 +16,19 @@ use strict;
 
 @ISA = qw(Bio::Parse::Record);
 
+my $BLANK           = '^\s*$';
+my $COMMENT         = '^\s*\#';
 
-my $Plain_Null           = '^\s*$';
-my $Plain_Comment        = '^\s*\#';
+#subrecords
+my $ALIGNMENT_BEGIN = '^\s*\S+\s+\S';
+my $ALIGNMENT_GROUP = '^\s*(\S+)\s+(\S+)\s*$';
+my $ALIGNMENT_END   = $BLANK;
 
-#delimit full Plain entry
-my $Plain_START          = '^\s*\S+\s+\S';
-my $Plain_END            = $Plain_Null;
+#full record
+my $DATA_BEGIN      = $ALIGNMENT_BEGIN;
+my $DATA_END        = $ALIGNMENT_END;
 
-#Plain record types
-my $Plain_ALIGNMENT      = '^\s*\S+\s+\S';
-my $Plain_ALIGNMENTend   = $Plain_END;
-
+my $DEBUG = 0;
 
 #Consume one entry-worth of input on text stream associated with $file and
 #return a new Plain instance.
@@ -35,19 +36,38 @@ sub get_entry {
     my ($parent) = @_;
     my ($line, $offset, $bytes) = ('', -1, 0);
 
+    #warn "BEGIN ENTRY\n"  if $DEBUG;
+
     while ($parent->{'text'}->getline(\$line)) {
 
-        #start of entry
-        if ($line =~ /$Plain_START/o and $offset < 0) {
+        #initial blank
+        if ($line =~ /$BLANK/o and $offset < 0) {
+            #warn " LEADING BLANK\n"  if $DEBUG;
+            next;
+        }
+
+        #initial comment
+        if ($line =~ /$COMMENT/o and $offset < 0) {
+            #warn " LEADING COMMENT\n"  if $DEBUG;
+            next;
+        }
+
+        #start of data
+        if ($line =~ /$DATA_BEGIN/o and $offset < 0) {
+            #warn " START OF DATA\n"  if $DEBUG;
             $offset = $parent->{'text'}->startofline;
             next;
         }
 
-        #end of entry
-        if ($line =~ /$Plain_END/o and ! $offset < 0) {
+        #end of data
+        if ($line =~ /$DATA_END/o and ! ($offset < 0)) {
+            #warn " END OF DATA\n"  if $DEBUG;
             last;
         }
     }
+
+    #warn "END ENTRY\n"  if $DEBUG;
+
     return 0   if $offset < 0;
 
     $bytes = $parent->{'text'}->tell - $offset;
@@ -68,25 +88,27 @@ sub new {
     $self = new Bio::Parse::Record($type, $parent, $text, $offset, $bytes);
     $text = new Bio::Parse::Scanner($self);
 
+    #warn "BEGIN PARSE\n"  if $DEBUG;
+
     while (defined ($line = $text->next_line)) {
 
-        #consume data
+        #comment: ignore in middle of alignment; test first
+        next    if $line =~ /$COMMENT/o;
 
-        #comment: ignore
-        next    if $line =~ /$Plain_Comment/o;
-
-        #ALIGNMENT lines
-        if ($line =~ /$Plain_ALIGNMENT/o) {
-            $text->scan_until($Plain_ALIGNMENTend, 'ALIGNMENT');
+        #alignment line
+        if ($line =~ /$ALIGNMENT_BEGIN/o) {
+            $text->scan_until($ALIGNMENT_END, 'ALIGNMENT');
             next;
         }
 
-        #blank line or empty record: ignore
-        next    if $line =~ /$Plain_Null/o;
+        #blank: ignore; test last
+        next    if $line =~ /$BLANK/o;
 
-        #default
         $self->warn("unknown field: $line");
     }
+
+    #warn "END PARSE\n"  if $DEBUG;
+
     $self;#->examine;
 }
 
@@ -115,10 +137,17 @@ sub new {
 
     my $off = 0;
 
+    #warn "BEGIN PARSE ALIGNMENT\n"  if $DEBUG;
+    #warn "POSITION(self): $offset $bytes\n" if $DEBUG;
+    #warn "POSITION(text): ", $text->{'offset'}, " ", $text->{'bytes'}, "\n" if $DEBUG;
+
     while (defined ($line = $text->next_line(1))) {
 
-        #id/sequence
-        if ($line =~ /^\s*(\S+)\s+(\S+)\s*$/o) {
+        #comment: ignore in middle of alignment; must come first
+        next    if $line =~ /$COMMENT/o;
+
+        #id sequence
+        if ($line =~ /$ALIGNMENT_GROUP/o) {
             $self->test_args(\$line, $1, $2);
             push @{$self->{'id'}}, $1    unless exists $self->{'seq'}->{$1};
             $self->{'seq'}->{$1} .= $2;
@@ -128,6 +157,8 @@ sub new {
         #default
         $self->warn("unknown field: $line");
     }
+
+    #warn "END PARSE ALIGNMENT\n"  if $DEBUG;
 
     #line length check
     if (defined $self->{'id'}->[0]) {
